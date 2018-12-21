6de3087f04da0acd9744070f5c682d71bc52267e
WCUBAS <wcubas.ugt@gmail.com>
2018-12-21 15:47:35 -0500

    prueba git

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 require('dotenv').config();
-const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+//const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(express.static('./public/web'));
+//app.use(bodyParser.json());
+//app.use(express.static('./public/web'));
 
 const port = 3000 || process.env.PORT;
-
-const assistant = new AssistantV1({
-  username: "14cbe5b4-074b-4ae8-a2c5-8bb976fef4b4",
-  password: "TM2fiXaCYkdO",
-  url: 'https://gateway.watsonplatform.net/assistant/api/',
-  version: '2018-07-07',
-});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -23,11 +16,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.post('/prueba/', (req, res) => {
+app.get('/prueba/', (req, res) => {
   res.send("HOLA");
 });
 
+app.listen(port, () => console.log(`Running on port ${port}`));
 
+/*
 app.post('/conversation/', (req, res) => {
   const { text, context = {} } = req.body;
 
@@ -43,5 +38,5 @@ app.post('/conversation/', (req, res) => {
     res.json(response);
   });
 });
+*/
 
-//app.listen(port, () => console.log(`Running on port ${port}`));
